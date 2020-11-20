411097a7e4905a4b392661855244e6cae7339504
joserneto7 <61655168+joserneto7@users.noreply.github.com>
2020-11-20 17:48:30 -0300

    Update sculptor.h

--- sculptor.h
+++ sculptor.h
@@ -23,15 +23,14 @@ private:
 
 public:
     //Alocação da Matriz 3D.
-    Sculptor(int _nx=0, int _ny=0, int _nz=0);
     /**
     * @brief Função para alocar a matriz necessária para esculpir os desenhos.
     */
+    Sculptor(int _nx=0, int _ny=0, int _nz=0);
 
     //Liberar a memória utilizada.
     ~Sculptor();
 
-    void setColor(float _r, float _g, float _b, float alpha);
     /**
      * @brief Atribuição das cores do objeto que está sendo desenvolvido.
      * @param _r,_g,_b são os parâmetros para definir a quantidade de vermelho, verde e azul, respectivamente, do que está
@@ -39,33 +38,33 @@ public:
      * completa da cor e o máximo da da cor.
      * @param alpha define a transparência do objeto, variando também de zero à um, conforme exposto acima.
       */
+    void setColor(float _r, float _g, float _b, float alpha);
 
-    void putVoxel(int x, int y, int z);
     /**
      * @brief Adiciona um Voxel (Um bloco construtor) dentro da matriz
      * @param x,y,z representam as cordenadas espaciais de onde você quer inserir esse voxel dentro da matriz.
       */
+    void putVoxel(int x, int y, int z);
 
-    void cutVoxel(int x, int y, int z);
     /**
      * @brief Essa função tem por finalidade desconstruir um voxel, já explicado de dentro da matriz.
      * @param x,y,z são as coordenadas espaciais do voxel que se quer cortar
        */
+    void cutVoxel(int x, int y, int z);
 
-    void putBox(int x0, int y0, int z0, int x1, int y1, int z1);
     /**
       * @brief Cria uma sequência de Voxels em forma de um cubo.
       * @param x0,y0,z0 são as coordenadas iniciais de onde será construído.
       * @param x1,y1,z1 delimitam até qual coordenada o cubo será construído
       */
+    void putBox(int x0, int y0, int z0, int x1, int y1, int z1);
 
-    void cutBox(int x0, int y0, int z0, int x1, int y1, int z1);
     /**
       * @brief Corta uma sequência de Voxels em forma de cubo.
       * @param Os parâmetros possuem o mesmo significado dos encontrados na função @sa putBox, mas dessa vez para desconstruir.
       */
+    void cutBox(int x0, int y0, int z0, int x1, int y1, int z1);
 
-    void putSphere(int xcenter, int ycenter, int zcenter, int radius);
     /**
       * @brief Criar uma sequência de Voxels em forma de esfera.
       * @param xcenter é o centro da esfera na coordenada x;
@@ -73,34 +72,36 @@ public:
       * @param zcenter é o centro da esfera na coordenada z;
       * @param radius é o raio da esfera.
       */
+    void putSphere(int xcenter, int ycenter, int zcenter, int radius);
 
-    void cutSphere(int xcenter, int ycenter, int zcenter, int radius);
     /**
       * @brief Cortar uma sequência de Voxels em forma de esfera.
       * @param são os mesmos da @sa putSphere
       */
+    void cutSphere(int xcenter, int ycenter, int zcenter, int radius);
 
-    void putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
     /**
       * @brief Criar uma sequência de Voxels em forma de elipsóide.
       * @param xcenter,ycenter,zcenter representam o centro da elipsóide nas componentes x, y e z da matriz
       * @param rx,ry,rz representam o raio do elipsóide nas componentes x, y e z da matriz
       */
+    void putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
 
-    void cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
     /**
       @brief Cortar uma sequência de Voxels, Em forma de elipsóide.
       @param são os mesmo da @sa putEllipsoid
         */
+    void cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
 
-    void writeOFF(string filename);
     /**
       @brief Criar arquivo para leitura do desenho, contendo as dimensões e as cores.
       */
+    void writeOFF(string filename);
 
-    void writeVECT(string filename);
     /**
       @brief Criar arquivo para armazenar quantidades de poligonos,vertices e posições.
       */
+    void writeVECT(string filename);
+
 };
 #endif // SCULPTOR_H
